5fdcb39bf4bec2c7fd400fed7ac0cd18687ea11e
Mudassir Ahmad <ma6386731@gmail.com>
2025-08-01 14:59:00 +0500

    another commit

--- src/component/BreadCrum.jsx
+++ src/component/BreadCrum.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import arrow_icon from '../assets/breadcrum_arrow.png'
+
 const BreadCrum = (props) => {
-    const {product} = props;
+  const { product } = props;
+
   return (
-    <div className='flex mt-10 gap-3'>
-   HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name} 
+    <div className='flex flex-wrap items-center mt-10 gap-2 sm:gap-3 text-sm sm:text-base px-4'>
+      <span className='whitespace-nowrap'>HOME</span>
+      <img src={arrow_icon} alt="arrow" className='w-3 h-3 sm:w-4 sm:h-4' />
+      <span className='whitespace-nowrap'>SHOP</span>
+      <img src={arrow_icon} alt="arrow" className='w-3 h-3 sm:w-4 sm:h-4' />
+      <span className='whitespace-nowrap capitalize'>{product.category}</span>
+      <img src={arrow_icon} alt="arrow" className='w-3 h-3 sm:w-4 sm:h-4' />
+      <span className='whitespace-wrap  mr-10 flex capitalize'>{product.name}</span>
     </div>
   )
 }
